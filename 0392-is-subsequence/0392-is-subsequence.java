9e193cc120dfe46c0e5264ff339fd0d78f4b7208
Mohith <mohithkumar4225@gmail.com>
2026-08-13 18:26:28 +0530

    Time: 2 ms (73.7%), Space: 42.9 MB (44.62%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -5,6 +5,7 @@ class Solution {
         while(i < s.length() && j < t.length()){
             if(s.charAt(i) == t.charAt(j)){
                 i++;
+                j++;
             } else {
                 j++;
             }
