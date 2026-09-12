4d5a93302e52c33287fa978537aeae4c20dd1970
Mohith <mohithkumar4225@gmail.com>
2026-09-12 13:02:56 +0530

    Time: 20 ms (37.34%), Space: 92.1 MB (8.32%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        HashSet<Integer> answer = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(answer.contains(nums[i])){
+                return nums[i];
+            }
+            answer.add(nums[i]);
+        }
+        return -1;
+    }
+}
