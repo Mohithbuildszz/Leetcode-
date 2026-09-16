24bedb44b9d5526629964f1aa331efae57d07378
Mohith <mohithkumar4225@gmail.com>
2026-09-16 11:15:01 +0530

    Time: 0 ms (100%), Space: 44.8 MB (32.89%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,10 +1,12 @@
 class Solution {
     public void nextPermutation(int[] nums) {
        int n = nums.length;
+       //to find breaking point//
        int i = n-2;
        while(i >= 0 && nums[i] >= nums[i+1]){
         i--;
        }  
+       // Swap from the smallest breaking points
        if(i>=0){
        for(int j=n-1;j>i;j--){
         if(nums[j] > nums[i]){
@@ -15,6 +17,7 @@ class Solution {
                 }
             }
        }
+       //swap the remaining elements present..
        int start = i+1;
        int end = n -1;
        while(start < end){
