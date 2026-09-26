cd01484f633a41ace106c698c05b14c5f1d7e36b
Mohith <mohithkumar4225@gmail.com>
2026-09-26 23:07:50 +0530

    Time: 15 ms (59.61%), Space: 51.7 MB (19.61%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,6 +1,6 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-    int n=g.length;
+        int n=g.length;
         int m=s.length;
         int left = 0;
         int right=0;
